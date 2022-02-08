2646a8e5e5b0662a23577774cc95c025863c5056
Cal Leung <cleun007@gmail.com>
2022-02-08 13:59:25 -0800

    Fix test

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,10 +1,10 @@
-/* eslint-ignore @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports */
+/* eslint-disable @typescript-eslint/no-var-requires, @typescript-eslint/no-require-imports */
 import { colors as importableColors } from '.';
 
 const designTokens = require('./figma/tokens.json');
 
 describe('Colors', () => {
-  it('Importable colors match figma colors', () => {
+  it('importable colors match figma colors', () => {
     expect(importableColors.dark.background.default).toStrictEqual(
       designTokens.dark.colors.background.default.value,
     );
