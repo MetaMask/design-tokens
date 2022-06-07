2f6aa8abe544dc9ea7c8365b20e02b383cf85740
georgewrmarshall <george.marshall@consensys.net>
2022-06-07 15:57:50 -0700

    Updating comments from objects and moving to types interface

--- src/js/themes/lightTheme/colors.ts
+++ src/js/themes/lightTheme/colors.ts
@@ -1,66 +1,8 @@
 import { ThemeColors } from '../types';
 
-/* eslint-disable jsdoc/check-property-names, jsdoc/tag-lines */
 /**
- * MetaMask's Design System Colors
- *
- * NOTE - This documentation doesn't reflect the actual structure of the colors object. We are only documenting the common properties between light and dark themes.
- *
- * @property {string} background.default - For default neutral backgrounds
- * @property {string} background.alternative - For a subtle contrast option for neutral backgrounds. (Example: backdrop, header background)
- *
- * @property {string} text.default - For general text that takes main priority in the information hierarchy
- * @property {string} text.alternative - For a weaker contrast option for neutral text
- * @property {string} text.muted - For inactive or lowest priority text. (Example: placeholder)
- *
- * @property {string} icon.default - For default neutral icons
- * @property {string} icon.alternative - For a weaker contrast option for neutral icons
- * @property {string} icon.muted - For inactive or lowest priority icons
- *
- * @property {string} border.default - For default neutral borders with visible contrast. (Example: text inputs)
- * @property {string} border.muted - For a weaker contrast option for neutral borders.
- *
- * @property {string} overlay.default - For shading layers behind modality screens
- * @property {string} overlay.alternative - For a stronger shading layer option behind modality screens
- * @property {string} overlay.inverse - [Deprecated] Should be used for elements over an overlay
- *
- * @property {string} primary.default - For primary user action related elements
- * @property {string} primary.alternative - For the \"pressed\" state of interactive primary elements
- * @property {string} primary.muted - For lowest contrast background used in primary elements
- * @property {string} primary.disabled - [Deprecated] Should be used for disabled state
- * @property {string} primary.inverse - For elements used on top of primary/default. (Example: label of primary button, check in a checkbox)
- *
- * @property {string} secondary.default - [Deprecated] Should be used for any secondary actions. It should not be used for any negative connotations such as warnings or errors as it is quite closely tied to the MetaMask Fox
- * @property {string} secondary.alternative - [Deprecated] Should be used as an alternative to secondary.default for things such as hover states
- * @property {string} secondary.muted - [Deprecated] It’s a very low contrasting secondary variant for things such as alert backgrounds. secondary.muted and secondary.inverse should not be used together in a foreground and background combination
- * @property {string} secondary.disabled - [Deprecated] Should be used for all disabled secondary action components
- * @property {string} secondary.inverse - [Deprecated] Should be used only as the foreground element on top of primary/default and primary/alternative. It is intended to be the most contrasting color to primary/default. It should meet all AA and AAA accessibility standards such as the text or icon of a primary button
- *
- * @property {string} error.default - For high-level alert danger/critical elements. Used for text, background, icon or border
- * @property {string} error.alternative - For the \"pressed\" state of interactive danger/critical elements
- * @property {string} error.muted - For lowest contrast background used in high-level alert danger/critical elements. (Example: notification background)
- * @property {string} error.disabled - [Deprecated] Should be used for disabled state
- * @property {string} error.inverse - For elements used on top of error/default (Example: label of danger/critical button)
- *
- * @property {string} warning.default - For low-mid level alert elements. Used for text, background, icon or border
- * @property {string} warning.alternative - [Deprecated] Should be used as an alternative to warning/default for things like hover or pressed states
- * @property {string} warning.muted - For lowest contrast background used in warning elements. (Example: notification background)
- * @property {string} warning.disabled - [Deprecated] Should be used for disabled state
- * @property {string} warning.inverse - For elements used on top of warning/default. Used for text, icon or border
- *
- * @property {string} success.default - For positive & good semantic elements. Used for text, background, icon or border
- * @property {string} success.alternative - [Deprecated] Should be used as an alternative to success/default for things like hover or pressed states
- * @property {string} success.muted - For lowest contrast background used in success semantic. (Example: notification background)
- * @property {string} success.disabled - [Deprecated] Should be used for disabled state
- * @property {string} success.inverse -  For elements used on top of success/default. Used for text, icon or border
- *
- * @property {string} info.default - For informational semantic elements. Used for text, background, icon or border
- * @property {string} info.alternative - [Deprecated] Should be used as an alternative to info/default for things like hover or pressed states
- * @property {string} info.muted - For lowest contrast background used in informational semantic. (Example: notification background)
- * @property {string} info.disabled - [Deprecated] Should be used for disabled state
- * @property {string} info.inverse - SFor elements used on top of info/default. Used for text, icon or border
+ * Light theme colors
  */
-/* eslint-enable jsdoc/check-property-names, jsdoc/tag-lines */
 
 export const colors: ThemeColors = {
   background: {
--- src/js/themes/types.ts
+++ src/js/themes/types.ts
@@ -2,68 +2,197 @@ import { ThemeTypography } from '../typography';
 
 export interface ThemeColors {
   background: {
+    /**
+     * {string} background.default - For default neutral backgrounds
+     */
     default: string;
+    /**
+     * {string} background.alternative - For a subtle contrast option for neutral backgrounds. (Example: backdrop, header background)
+     */
     alternative: string;
   };
   text: {
+    /**
+     * {string} text.default - For general text that takes main priority in the information hierarchy
+     */
     default: string;
+    /**
+     * {string} text.alternative - For a weaker contrast option for neutral text
+     */
     alternative: string;
+    /**
+     * {string} text.muted - For inactive or lowest priority text. (Example: placeholder)
+     */
     muted: string;
   };
   icon: {
+    /**
+     * {string} icon.default - For default neutral icons
+     */
     default: string;
+    /**
+     * {string} icon.alternative - For a weaker contrast option for neutral icons
+     */
     alternative: string;
+    /**
+     * {string} icon.muted - For inactive or lowest priority icons
+     */
     muted: string;
   };
   border: {
+    /**
+     * {string} border.default - For default neutral borders with visible contrast. (Example: text inputs)
+     */
     default: string;
+    /**
+     * {string} border.muted - For a weaker contrast option for neutral borders.
+     */
     muted: string;
   };
   overlay: {
+    /**
+     * {string} overlay.default - For shading layers behind modality screens
+     */
     default: string;
+    /**
+     * {string} overlay.alternative - For a stronger shading layer option behind modality screens
+     */
     inverse: string;
+    /**
+     * {string} overlay.inverse - [Deprecated] Should be used for elements over an overlay
+     */
     alternative: string;
   };
   primary: {
+    /**
+     * {string} primary.default - For primary user action related elements
+     */
     default: string;
+    /**
+     * {string} primary.alternative - For the \"pressed\" state of interactive primary elements
+     */
     alternative: string;
+    /**
+     * {string} primary.muted - For lowest contrast background used in primary elements
+     */
     muted: string;
+    /**
+     * {string} primary.inverse - For elements used on top of primary/default. (Example: label of primary button, check in a checkbox)disabled state
+     */
     inverse: string;
+    /**
+     * {string} primary.disabled - [Deprecated] Should be used for
+     */
     disabled: string;
   };
   secondary: {
+    /**
+     * {string} secondary.default - [Deprecated] Should be used for any secondary actions. It should not be used for any negative connotations such as warnings or errors as it is quite closely tied to the MetaMask Fox
+     */
     default: string;
+    /**
+     * {string} secondary.alternative - [Deprecated] Should be used as an alternative to secondary.default for things such as hover states
+     */
     alternative: string;
+    /**
+     * {string} secondary.muted - [Deprecated] It’s a very low contrasting secondary variant for things such as alert backgrounds. secondary.muted and secondary.inverse should not be used together in a foreground and background combination
+     */
     muted: string;
+    /**
+     * {string} secondary.inverse - [Deprecated] Should be used only as the foreground element on top of primary/default and primary/alternative. It is intended to be the most contrasting color to primary/default. It should meet all AA and AAA accessibility standards such as the text or icon of a primary button
+     */
     inverse: string;
+    /**
+     * {string} secondary.disabled - [Deprecated] Should be used for all disabled secondary action components
+     */
     disabled: string;
   };
   error: {
+    /**
+     * {string} error.default - For high-level alert danger/critical elements. Used for text, background, icon or border
+     */
     default: string;
+    /**
+     * {string} error.alternative - For the \"pressed\" state of interactive danger/critical elements
+     */
     alternative: string;
+    /**
+     * {string} error.muted - For lowest contrast background used in high-level alert danger/critical elements. (Example: notification background)
+     */
     muted: string;
+    /**
+     * {string} error.inverse - For elements used on top of error/default (Example: label of danger/critical button)
+     */
     inverse: string;
+    /**
+     * {string} error.disabled - [Deprecated] Should be used for disabled state
+     */
     disabled: string;
   };
   warning: {
+    /**
+     * {string} warning.muted - For lowest contrast background used in warning elements. (Example: notification background)
+     */
     default: string;
+    /**
+     * {string} warning.alternative - [Deprecated] Should be used as an alternative to warning/default for things like hover or pressed states
+     */
     alternative: string;
+    /**
+     * {string} warning.muted - For lowest contrast background used in warning elements. (Example: notification background)
+     */
     muted: string;
+    /**
+     * {string} warning.inverse - For elements used on top of warning/default. Used for text, icon or border
+     */
     inverse: string;
+    /**
+     * {string} warning.disabled - [Deprecated] Should be used for disabled state
+     */
     disabled: string;
   };
   success: {
+    /**
+     * {string} success.default - For positive & good semantic elements. Used for text, background, icon or border
+     */
     default: string;
+    /**
+     * {string} success.alternative - [Deprecated] Should be used as an alternative to success/default for things like hover or pressed states
+     */
     alternative: string;
+    /**
+     * {string} success.muted - For lowest contrast background used in success semantic. (Example: notification background)
+     */
     muted: string;
+    /**
+     * {string} success.inverse -  For elements used on top of success/default. Used for text, icon or border
+     */
     inverse: string;
+    /**
+     * {string} success.disabled - [Deprecated] Should be used for disabled state
+     */
     disabled: string;
   };
   info: {
+    /**
+     * {string} info.default - For informational semantic elements. Used for text, background, icon or border
+     */
     default: string;
+    /**
+     * {string} info.alternative - [Deprecated] Should be used as an alternative to info/default for things like hover or pressed states
+     */
     alternative: string;
+    /**
+     * {string} info.muted - For lowest contrast background used in informational semantic. (Example: notification background)
+     */
     muted: string;
+    /**
+     * {string} info.inverse - SFor elements used on top of info/default. Used for text, icon or border
+     */
     inverse: string;
+    /**
+     * {string} info.disabled - [Deprecated] Should be used for disabled state
+     */
     disabled: string;
   };
 }
