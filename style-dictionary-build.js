f4cebc8f0d0122d1d0cd75e87033325d244f952f
garrettbear <gwhisten@gmail.com>
2024-02-07 15:40:09 -0800

    broken out into brand light and dark

--- style-dictionary-build.js
+++ style-dictionary-build.js
@@ -3,13 +3,6 @@ import StyleDictionaryModule from 'style-dictionary';
 // Attempt to import everything under a namespace from '@tokens-studio/sd-transforms'
 import * as sdTransforms from '@tokens-studio/sd-transforms';
 
-// Assuming sdTransforms applies directly as a function or setup call
-// If sdTransforms is an object with methods, you might need to call a specific method instead
-// This example directly uses sdTransforms assuming it's a function or has a default method for application
-// Adjust based on actual structure which you can determine by inspecting the sdTransforms object
-
-// Apply sdTransforms to StyleDictionary
-// This line might need adjustment based on the actual API of sdTransforms
 const StyleDictionary = sdTransforms.default
   ? sdTransforms.default(StyleDictionaryModule)
   : StyleDictionaryModule;
@@ -18,23 +11,39 @@ const StyleDictionary = sdTransforms.default
 StyleDictionary.registerFormat({
   name: 'custom/cssVariables',
   formatter({ dictionary }) {
+    const baseTokens = [];
+    const lightThemeTokens = [];
+    const darkThemeTokens = [];
+
+    dictionary.allProperties.forEach(token => {
+      // Convert token names to CSS variable names
+      const tokenName = token.name.replace(/([A-Z])/g, (match) => `-${match.toLowerCase()}`);
+      const cssVariable = `  --${tokenName}: ${token.value};`;
+
+      if (token.name.includes('light')) {
+        lightThemeTokens.push(cssVariable);
+      } else if (token.name.includes('dark')) {
+        darkThemeTokens.push(cssVariable);
+      } else {
+        baseTokens.push(cssVariable);
+      }
+    });
+
     return `/**
  * Do not edit directly
  * Generated on ${new Date().toUTCString()}
  */
 :root {
-${dictionary.allProperties
-  .map((token) => {
-    // Convert token names to CSS variable names
-    const tokenName = token.name.replace(
-      /([A-Z])/g,
-      (match) => `-${match.toLowerCase()}`,
-    );
-    return `  --${tokenName}: ${token.value};`;
-  })
-  .join('\n')}
+${baseTokens.join('\n')}
 }
-`;
+
+:root {
+${lightThemeTokens.join('\n')}
+}
+
+[data-theme='dark'] {
+${darkThemeTokens.join('\n')}
+}`;
   },
 });
 
