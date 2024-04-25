14eb56f32bb722dd29a1b212f8abfbc23d4be004
Garrett Bear <gwhisten@gmail.com>
2024-04-25 14:24:41 -0700

    feat: brandColors updated to figma variable json (#663)
    * chore: inital add of blank json file
    
    ```
    - Blue updates
     - blue000 no longer exist
     - blue025 added
     - blue 050 added
    
    - Orange updates
     - orange000 no longer exist
     - orange 025 added
     - orange 050 added
    
    - Green updates
     - green000 no longer exist
     - green025 added
     - green 050 added
    
    - Red updates
     - red000 no longer exist
     - red025 added
     - red050 added
    
    - Yellow updates
     - yellow000 no longer exist
     - yellow025 added
     - yellow050 added
     - yellow700 added
     - yellow800 added
     - yellow900 added
    
    - Violet no longer exist
    
    - Purple added
    
    - Lime added
    ```
    
    ---------
    
    Co-authored-by: georgewrmarshall <george.marshall@consensys.net>

--- docs/components/ColorSwatchGroup/ColorSwatchGroup.tsx
+++ docs/components/ColorSwatchGroup/ColorSwatchGroup.tsx
@@ -1,4 +1,4 @@
-import React, { FunctionComponent } from 'react';
+import React from 'react';
 import { Theme } from '../../utils/useJsonColor';
 import { ColorSwatch } from '..';
 
@@ -11,50 +11,48 @@ interface ColorSwatchGroupProps {
    * The color of text background that contains the name of the color defaults to background.default
    */
   textBackgroundColor?: string;
-  /**
-   * The border color of the swatch defaults to border.muted
-   */
   borderColor?: string;
-  /**
-   * The color of the text defaults to text.default
-   */
   textColor?: string;
-  /**
-   * The name of the color
-   */
-  name?: string;
 }
 
-export const ColorSwatchGroup: FunctionComponent<ColorSwatchGroupProps> = ({
+export const ColorSwatchGroup: React.FC<ColorSwatchGroupProps> = ({
   swatchData,
   borderColor,
-  textBackgroundColor,
-  textColor,
+  textBackgroundColor = 'transparent',
+  textColor = '#212529',
 }) => {
   if (!swatchData) {
     return <div>No swatch data</div>;
   }
-  const swatchColorsArr = Object.keys(swatchData);
+
+  // Function to extract numbers and sort them numerically
+  const sortShadesNumerically = (a, b) => {
+    const numberPattern = /\d+/; // Matches digits in the shade identifier
+    const numberA = parseInt(a.match(numberPattern), 10);
+    const numberB = parseInt(b.match(numberPattern), 10);
+    return numberA - numberB;
+  };
+
+  // Function to determine the contrast of the text color based on the background color
+  function getContrastYIQ(hexcolor) {
+    hexcolor = hexcolor.replace('#', '');
+    var r = parseInt(hexcolor.substr(0, 2), 16);
+    var g = parseInt(hexcolor.substr(2, 2), 16);
+    var b = parseInt(hexcolor.substr(4, 2), 16);
+    var yiq = (r * 299 + g * 587 + b * 114) / 1000;
+    return yiq >= 128 ? 'black' : 'white';
+  }
 
   const renderSwatches = () => {
-    return swatchColorsArr.map((category) => {
-      const colorsObj = swatchData[category];
-      let colorsArr: any = [];
-      const recursiveColors = (nextLevel, label) => {
-        for (const key in nextLevel) {
-          const level = nextLevel[key];
-          if (level.value) {
-            colorsArr.push({
-              label: `${label}${key}`,
-              value: level.value,
-              description: level.description,
-            });
-            continue;
-          }
-          recursiveColors(level, `${label}${key}.`);
-        }
-      };
-      recursiveColors(colorsObj, '');
+    return Object.entries(swatchData).map(([category, colors]) => {
+      // Filter out color keys that contain a dash, indicating a percentage-based color
+      const filteredColorKeys = Object.keys(colors).filter(
+        (key) => !key.includes('-'),
+      );
+
+      // Sort the remaining color keys numerically
+      const sortedColorKeys = filteredColorKeys.sort(sortShadesNumerically);
+
       return (
         <div
           key={category}
@@ -72,17 +70,19 @@ export const ColorSwatchGroup: FunctionComponent<ColorSwatchGroupProps> = ({
               gridTemplateColumns: 'repeat(auto-fill, 300px)',
             }}
           >
-            {colorsArr.map((color) => {
+            {sortedColorKeys.map((label) => {
+              const { value, description } = colors[label];
               return (
-                <div key={color.label}>
+                <div key={label}>
                   <ColorSwatch
-                    color={color.value}
-                    name={color.label}
-                    {...{ borderColor, textBackgroundColor, textColor }}
+                    color={value}
+                    name={label}
+                    textColor={getContrastYIQ(value)}
+                    {...{ borderColor, textBackgroundColor }}
                   />
-                  {color?.description ? (
-                    <p style={{ lineHeight: 1.3 }}>{color?.description}</p>
-                  ) : null}
+                  {description && (
+                    <p style={{ lineHeight: 1.3 }}>{description}</p>
+                  )}
                 </div>
               );
             })}
--- src/js/brandColorBrandEvolution/brandColorBrandEvolution.types.ts
+++ src/js/brandColorBrandEvolution/brandColorBrandEvolution.types.ts
@@ -1,155 +1,155 @@
-export type BrandColor = {
-  /** grey/grey100: #d6d9dc */
+export type BrandColorBrandEvolution = {
+  /** grey/grey100: #dadce5 */
   grey100: string;
-  /** grey/grey200: #bbc0c5 */
+  /** grey/grey200: #b7bbc8 */
   grey200: string;
-  /** grey/grey300: #9fa6ae */
+  /** grey/grey300: #9ca1af */
   grey300: string;
-  /** grey/grey400: #848c96 */
+  /** grey/grey400: #858b9a */
   grey400: string;
-  /** grey/grey500: #6a737d */
+  /** grey/grey500: #686e7d */
   grey500: string;
-  /** grey/grey600: #535a61 */
+  /** grey/grey600: #4b505c */
   grey600: string;
-  /** grey/grey700: #3b4046 */
+  /** grey/grey700: #31333a */
   grey700: string;
-  /** grey/grey800: #24272a */
+  /** grey/grey800: #222325 */
   grey800: string;
-  /** grey/grey900: #141618 */
+  /** grey/grey900: #121314 */
   grey900: string;
   /** grey/grey1000: #000000 */
   grey1000: string;
-  /** grey/grey050: #f2f4f6 */
+  /** grey/grey050: #f3f5f9 */
   grey050: string;
   /** grey/grey000: #ffffff */
   grey000: string;
-  /** grey/grey025: #fafbfc */
+  /** grey/grey025: #f7f9fc */
   grey025: string;
-  /** blue/blue100: #a7d9fe */
+  /** blue/blue100: #d6dbff */
   blue100: string;
-  /** blue/blue200: #75c4fd */
+  /** blue/blue200: #adb6fe */
   blue200: string;
-  /** blue/blue300: #43aefc */
+  /** blue/blue300: #8b99ff */
   blue300: string;
-  /** blue/blue400: #1098fc */
+  /** blue/blue400: #6f7eff */
   blue400: string;
-  /** blue/blue500: #0376c9 */
+  /** blue/blue500: #4459ff */
   blue500: string;
-  /** blue/blue600: #0260a4 */
+  /** blue/blue600: #2c3dc5 */
   blue600: string;
-  /** blue/blue700: #024272 */
+  /** blue/blue700: #1c277f */
   blue700: string;
-  /** blue/blue800: #01253f */
+  /** blue/blue800: #131b59 */
   blue800: string;
-  /** blue/blue900: {blue.blue900} */
+  /** blue/blue900: #0b0f32 */
   blue900: string;
-  /** blue/blue050: #eaf6ff */
+  /** blue/blue050: #f4f5ff */
   blue050: string;
-  /** blue/blue025: #eaf6ff */
+  /** blue/blue025: #f8f9ff */
   blue025: string;
-  /** green/green100: #afecbd */
+  /** green/green100: #c1e6c9 */
   green100: string;
-  /** green/green200: #5dd879 */
+  /** green/green200: #80cb91 */
   green200: string;
-  /** green/green300: #28a745 */
+  /** green/green300: #4cb564 */
   green300: string;
-  /** green/green400: #28a745 */
+  /** green/green400: #279f41 */
   green400: string;
-  /** green/green500: #1c8234 */
+  /** green/green500: #1c7e33 */
   green500: string;
-  /** green/green600: #145523 */
+  /** green/green600: #145d25 */
   green600: string;
-  /** green/green700: #145523 */
+  /** green/green700: #0d3b18 */
   green700: string;
-  /** green/green800: #0a2c12 */
+  /** green/green800: #092912 */
   green800: string;
-  /** green/green900: #041007 */
+  /** green/green900: #051709 */
   green900: string;
-  /** green/green050: #d6ffdf */
+  /** green/green050: #ebf7ee */
   green050: string;
-  /** green/green025: #f3fcf5 */
+  /** green/green025: #f1faf4 */
   green025: string;
-  /** red/red100: #f7d5d8 */
+  /** red/red100: #ffd2d5 */
   red100: string;
-  /** red/red200: #f1b9be */
+  /** red/red200: #ffa1aa */
   red200: string;
-  /** red/red300: #e88f97 */
+  /** red/red300: #ff7584 */
   red300: string;
-  /** red/red400: #e06470 */
+  /** red/red400: #f74d5e */
   red400: string;
-  /** red/red500: #d73847 */
+  /** red/red500: #ca3542 */
   red500: string;
-  /** red/red600: #8e1d28 */
+  /** red/red600: #952731 */
   red600: string;
-  /** red/red700: #64141c */
+  /** red/red700: #611a20 */
   red700: string;
-  /** red/red800: #3a0c10 */
+  /** red/red800: #431216 */
   red800: string;
-  /** red/red900: #3a0c10 */
+  /** red/red900: #3b0f13 */
   red900: string;
-  /** red/red050: #fcf2f3 */
+  /** red/red050: #fff2f3 */
   red050: string;
-  /** red/red025: #fcf2f3 */
+  /** red/red025: #fff6f7 */
   red025: string;
-  /** yellow/yellow100: #ffdf70 */
+  /** yellow/yellow100: #ffd957 */
   yellow100: string;
-  /** yellow/yellow200: #ffc70a */
+  /** yellow/yellow200: #f0b034 */
   yellow200: string;
-  /** yellow/yellow300: #f8883b */
+  /** yellow/yellow300: #e18f01 */
   yellow300: string;
-  /** yellow/yellow400: #f66a0a */
+  /** yellow/yellow400: #c37b02 */
   yellow400: string;
-  /** yellow/yellow500: #bf5208 */
+  /** yellow/yellow500: #9a6300 */
   yellow500: string;
-  /** yellow/yellow600: #954005 */
+  /** yellow/yellow600: #714802 */
   yellow600: string;
-  /** yellow/yellow700: #632b04 */
+  /** yellow/yellow700: #482d01 */
   yellow700: string;
-  /** yellow/yellow800: #321602 */
+  /** yellow/yellow800: #321f00 */
   yellow800: string;
-  /** yellow/yellow900: #321602 */
+  /** yellow/yellow900: #1c1100 */
   yellow900: string;
-  /** yellow/yellow050: #fff2c5 */
+  /** yellow/yellow050: #fff5d5 */
   yellow050: string;
-  /** yellow/yellow025: #fefcde */
+  /** yellow/yellow025: #fff9e6 */
   yellow025: string;
-  /** orange/orange100: #fbc49d */
+  /** orange/orange100: #ffd4c1 */
   orange100: string;
-  /** orange/orange200: #faa66c */
+  /** orange/orange200: #ffa47c */
   orange200: string;
-  /** orange/orange300: #f8883b */
+  /** orange/orange300: #ff7940 */
   orange300: string;
-  /** orange/orange400: #f66a0a */
+  /** orange/orange400: #f35714 */
   orange400: string;
-  /** orange/orange500: #bf5208 */
+  /** orange/orange500: #c3420d */
   orange500: string;
-  /** orange/orange600: #954005 */
+  /** orange/orange600: #932d06 */
   orange600: string;
-  /** orange/orange700: #632b04 */
+  /** orange/orange700: #631800 */
   orange700: string;
-  /** orange/orange800: #321602 */
+  /** orange/orange800: #451100 */
   orange800: string;
-  /** orange/orange900: #321602 */
+  /** orange/orange900: #280a00 */
   orange900: string;
-  /** orange/orange050: #fde2cf */
+  /** orange/orange050: #fff0ea */
   orange050: string;
-  /** orange/orange025: #fef5ef */
+  /** orange/orange025: #fff8f5 */
   orange025: string;
   /** purple/purple100: #efd2ff */
   purple100: string;
-  /** purple/purple200: {violet.violet300} */
+  /** purple/purple200: #dfa4ff */
   purple200: string;
   /** purple/purple300: #d27dff */
   purple300: string;
   /** purple/purple400: #b864f5 */
   purple400: string;
-  /** purple/purple500: {purple.purple500} */
+  /** purple/purple500: #8f44e4 */
   purple500: string;
   /** purple/purple600: #6c2ab2 */
   purple600: string;
-  /** purple/purple700: #4c1178 */
+  /** purple/purple700: #6c2ab2 */
   purple700: string;
-  /** purple/purple800: #32054d */
+  /** purple/purple800: #380658 */
   purple800: string;
   /** purple/purple900: #280a00 */
   purple900: string;
@@ -171,7 +171,7 @@ export type BrandColor = {
   lime600: string;
   /** lime/lime700: #093a31 */
   lime700: string;
-  /** lime/lime800: #022321 */
+  /** lime/lime800: #012826 */
   lime800: string;
   /** lime/lime900: #011515 */
   lime900: string;
