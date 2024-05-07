75e96b1f58b1aab0d0e4ad568df7e8395c058554
garrettbear <gwhisten@gmail.com>
2024-05-07 10:05:20 -0700

    feat: working version of using brandEvolution

--- automation-scripts/themeColors.js
+++ automation-scripts/themeColors.js
@@ -5,7 +5,7 @@ const fs = require('fs').promises;
  * @param {string} inputFilePath - The path to the JSON file containing light theme color definitions.
  * @param {string} colorsOutputFilePath - The path where the JavaScript colors object file should be written.
  */
-async function generateLightThemeFiles(inputFilePath, colorsOutputFilePath) {
+async function generateThemeFiles(inputFilePath, colorsOutputFilePath) {
   try {
     const data = await fs.readFile(inputFilePath, 'utf8');
     const colorData = JSON.parse(data);
@@ -19,9 +19,9 @@ async function generateLightThemeFiles(inputFilePath, colorsOutputFilePath) {
       Object.entries(colorDetails).forEach(([shade, details]) => {
         // Remove asterisks, convert kebab-case to camelCase, and simplify certain names
         let cleanShade = shade
-          .replace(/\*/g, '')
-          .replace(/-([a-z])/gi, (_, p1) => p1.toUpperCase());
-        cleanShade = cleanShade.replace(/\s*\(strong\)\s*/gi, '');
+          .replace(/\*/gu, '')
+          .replace(/-([a-z])/giu, (_, p1) => p1.toUpperCase());
+        cleanShade = cleanShade.replace(/\s*\(strong\)\s*/giu, '');
 
         let valueReference;
         // Match patterns with and without percentage modifiers
@@ -60,6 +60,12 @@ async function generateLightThemeFiles(inputFilePath, colorsOutputFilePath) {
   }
 }
 
-const inputFilePath = '../src/figma/lightTheme.json';
-const colorsOutputFilePath = '../src/js/themes/lightTheme/colors.ts';
-generateLightThemeFiles(inputFilePath, colorsOutputFilePath);
+// Light Theme
+// const inputFilePathLight = '../src/figma/lightTheme.json';
+// const colorsOutputFilePathLight = '../src/js/themes/lightTheme/colors.ts';
+// generateThemeFiles(inputFilePathLight, colorsOutputFilePathLight);
+
+// Dark Theme
+const inputFilePathDark = '../src/figma/darkTheme.json';
+const colorsOutputFilePathDark = '../src/js/themes/darkTheme/colors.ts';
+generateThemeFiles(inputFilePathDark, colorsOutputFilePathDark);
